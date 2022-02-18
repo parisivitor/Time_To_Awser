fd5653a0d94fa0e3995685242638325ba5340a56
Vitor Parisi <parisivitor95@gmail.com>
2022-02-18 10:52:34 -0300

    funcao para traduzir a model

--- time_to_answer/app/helpers/admins_backoffice_helper.rb
+++ time_to_answer/app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,8 @@
 module AdminsBackofficeHelper
+
+  def translate_attribute(object, method)
+    return object.model.human_attribute_name(method) if(object && method)
+    "Informe os parametros!"
+  end
+
 end
