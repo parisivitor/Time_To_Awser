677836a094ba2dbe7cf932f37e96125664c1ff8c
Vitor Parisi <parisivitor95@gmail.com>
2022-03-15 15:12:27 -0300

    buscar por assutos

--- time_to_answer/app/controllers/site/search_controller.rb
+++ time_to_answer/app/controllers/site/search_controller.rb
@@ -4,4 +4,8 @@ class Site::SearchController < SiteController
     # @questions = Question.includes(:answers).page(params[:page])
     @questions = Question._search_(params[:term], params[:page])
   end
+
+  def subject
+    @questions = Question._search_subject_(params[:subject_id], params[:page])
+  end
 end
--- time_to_answer/app/models/question.rb
+++ time_to_answer/app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  belongs_to :subject, inverse_of: :questions
+  belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
@@ -17,8 +17,24 @@ class Question < ApplicationRecord
   # end
 
   # Por scopes
-  scope :_search_, -> (term, page) {includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page) }
-  scope :ordenation_desc, -> (page) {includes(:answers).order('created_at desc').page(page)}
+  scope :_search_, -> (term, page) {
+      includes(:answers,  :subject)
+      .where("lower(description) LIKE ?", "%#{term.downcase}%")
+      .page(page)
+    }
+
+  scope :_search_subject_, -> (subject_id, page) {
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
+  }
+
+  scope :ordenation_desc, -> (page) {
+    includes(:answers,  :subject)
+    .order('created_at desc')
+    .page(page)
+  }
+
   scope :paginate, -> { page(page)}
 
 
