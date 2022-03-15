8fe908ae99408bb755e9400c67c1260b7272e528
Vitor Parisi <parisivitor95@gmail.com>
2022-03-15 15:13:18 -0300

    responder questoes

--- time_to_answer/app/helpers/site_helper.rb
+++ time_to_answer/app/helpers/site_helper.rb
@@ -1,2 +1,12 @@
 module SiteHelper
+  def msg_jumbotron
+    case params[:action]
+    when 'index'
+      "Resultado das ultimas perguntas cadastradas."
+    when 'questions'
+      "Resultado da pesquisa para o termo: \"#{params[:term]}\" "
+    when 'subject'
+      "Assunto: #{params[:subject]}"
+    end
+  end
 end
