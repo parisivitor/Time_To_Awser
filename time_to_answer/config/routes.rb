cbcaa28263dd52f013a145817270bcb3d2445095
Vitor Parisi <parisivitor95@gmail.com>
2022-03-15 15:13:39 -0300

    cadastro de usuarios

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
     get 'search', to: 'search#questions'
+    get 'subject/:subject_id/:subject', to: 'search#subject', as: 'search_subject'
     post 'answer', to: 'answer#question'
   end
   namespace :users_backoffice do
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
     resources :questions # Perguntas
   end
 
-  devise_for :admins
+  devise_for :admins, skip: [:registrations]
   devise_for :users
 
   get 'inicio', to: 'site/welcome#index'
