c29d58aeb24fccf5c8f2ae354270efde7f6def9b
Vitor Parisi <parisivitor95@gmail.com>
2022-03-16 14:36:42 -0300

    adicionando perfil do usuario e edição do mesmo

--- time_to_answer/app/models/user.rb
+++ time_to_answer/app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  def full_name
+    # "#{self.first_name} #{self.last_name}"
+    [self.first_name,self.last_name].join(' ')
+  end
 end
