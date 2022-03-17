9d9ffc8645000c04d1b63466fb0aeb0cdd2a1575
Vitor Parisi <parisivitor95@gmail.com>
2022-03-17 15:05:07 -0300

    estatisticas dos usuarios

--- time_to_answer/app/controllers/application_controller.rb
+++ time_to_answer/app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   layout :layout_by_resource
+  before_action :check_pagination
+
+
 
   protected
     def layout_by_resource
       return "#{resource_class.to_s.downcase}_devise" if devise_controller?
       "application"
     end
+
+    def check_pagination
+      unless user_signed_in?
+        params.extract!(:page)
+      end
+    end
+
 end
--- time_to_answer/db/schema.rb
+++ time_to_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_201104) do
+ActiveRecord::Schema.define(version: 2022_03_17_144446) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,15 @@ ActiveRecord::Schema.define(version: 2022_03_16_201104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
+  end
+
+  create_table "user_statistics", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "right_questions", default: 0
+    t.integer "wrong_question", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_statistics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
